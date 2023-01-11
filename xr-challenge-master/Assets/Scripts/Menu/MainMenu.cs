21442bc48a92204a9f0ce74d079b60b79c02c01c
NeatlyPackaged <92084795+NeatlyPackaged@users.noreply.github.com>
2023-01-11 19:40:07 +0000

    Fixed Resolution Text Issue

--- xr-challenge-master/Assets/Scripts/Menu/MainMenu.cs
+++ xr-challenge-master/Assets/Scripts/Menu/MainMenu.cs
@@ -39,7 +39,7 @@ public class MainMenu : MonoBehaviour
         int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "hz";
             options.Add(option);
 
             if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
@@ -51,6 +51,11 @@ public class MainMenu : MonoBehaviour
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+    }
+
+    void ClearResolutions()
+    {
+        resolutionDropdown.ClearOptions();
     }
 
     public void AudioPlay()
@@ -114,6 +119,7 @@ public class MainMenu : MonoBehaviour
     {
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        
     }
 
     public void SetVolume(float volume)
